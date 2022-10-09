391fa1ab67f24d8aa531f2a30b65edbcbd632a67
Mrfneto <mrfneto@gmail.com>
2022-10-09 18:38:39 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div class="min-h-screen bg-zinc-900 text-zinc-100">
+    <RouterView />
+  </div>
+</template>
---
+++ src/components/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div class="container py-16">
+    <h1 class="text-3xl font-bold">Home Page</h1>
+  </div>
+</template>
