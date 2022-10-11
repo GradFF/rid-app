d5e8e3a1ebdb7c72f8d7b43ef30353d1cad36b6d
Mrfneto <mrfneto@gmail.com>
2022-10-10 21:42:37 -0300

    env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,10 +3,20 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
-import { firebaseConfig } from './config'
+// import { firebaseConfig } from './config'
 /**
  *  create src/firebase/config.js
  */
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID
+}
 const app = initializeApp(firebaseConfig)
 
 const auth = getAuth(app)
